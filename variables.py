bcc8b2c354efea7502d59234569dbd09a95f9484
Mason Garcia <43070886+MasonMasoff@users.noreply.github.com>
2023-06-01 11:22:01 -0500

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,4 +1,4 @@
-organization = "qurius-inc"
+organization = "REPLACE WITH ORGANIZATION NAME"
 access_token = "REPLACE THIS WITH YOUR TOKEN"
 csv_file_path = "REPLACE THIS WITH YOUR FILEPATH/repository_list.csv"
 xlsx_file_path = "REPLACE THIS WITH YOUR FILEPATH/repository_list.xlsx"
